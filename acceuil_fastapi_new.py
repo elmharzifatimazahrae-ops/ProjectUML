902d808af61164ac4c8601405efaa25b1c3762e9
elmharzifatimazahrae-ops <elmharzifatimazahrae@gmail.com>
2026-05-17 21:06:46 +0100

    Update acceuil_fastapi_new.py

--- acceuil_fastapi_new.py
+++ acceuil_fastapi_new.py
@@ -558,5 +558,6 @@ def confirm_contact(request: ConfirmContactRequest):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8002)
+    port = int(os.environ.get("PORT", 8080))
+uvicorn.run(app, host="0.0.0.0", port=port)
 
